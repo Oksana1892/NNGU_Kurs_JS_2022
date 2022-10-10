7f99a135f27119544dabb0853575f43737451ee0
Oksana1892 <Sidorkurs@yandex.ru>
2022-10-10 22:36:09 +0300

    добавила файлы первого занятия

---
+++ index.js
@@ -0,0 +1,40 @@
+// console.log ('Hello world');
+
+// const ourPlanetName = 'Земля';
+// console.log(ourPlanetName);
+
+// let currentTime = '12:12';
+// console.log(currentTime);
+
+// let articlesCount = 8;
+// console.log(articlesCount);
+
+// let isCash = 'no';
+// let isCard = 'yes';
+// console.log(isCash);
+
+// const firstName = 'Горбунова';
+// const secondName = 'Оксана';
+// const thirdName = 'Владимировна';
+// console.log(firstName, secondName, thirdName);
+
+const name = 'Oksana';
+const age = 18;
+const userData = `имя пользовтеля: ${name}, возраст ${age}`;
+const userDatanew = 'имя пользователя: ${name}, возраст ${age}';
+console.log(userData);
+
+const isUser = false;
+const isVisible = true;
+const isVisible2 = true; 
+
+let townName;
+townName = 'Moscow';
+console.log(townName);
+
+const userData2 = {
+    name: 'Иван',
+    age: '34',
+}
+console.log(typeof userData2.age);
+console.log(typeof age);
